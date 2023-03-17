b6820b6f5d05d9a2eb424d1eb02c0a71337d0013
xxxRomanxxx <112776664+xxxRomanxxx@users.noreply.github.com>
2023-03-17 23:21:20 +0200

    JS completed

--- src/index.js
+++ src/index.js
@@ -1,62 +1,153 @@
-import fetchGallery  from "./js/fetchGallery";
-import axios from 'axios';
+import SearchApiPixabay  from "./js/fetchGallery";
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 
-const serchForm = document.querySelector('.search-form');
+const searchForm = document.querySelector('.search-form');
 const galleryItems = document.querySelector('.gallery');
 const loadMoreButton = document.querySelector('.load-more');
 
-serchForm.addEventListener('submit', onFormSubmit);
+const apiService = new SearchApiPixabay();
+
+searchForm.addEventListener('submit', onFormSubmit);
 loadMoreButton.addEventListener('click', onLoadMoreButton);
 
-let serchName = '';
-
-
+let simpleLightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 async function onFormSubmit(e) {
     e.preventDefault();
-    serchName = serchForm['searchQuery'].value;
+    const searchName = e.target.elements.searchQuery.value.trim();
+    if (!searchName) {
+      return Notiflix.Notify.failure(
+        'Request is not possible without entering data'
+      );
+    }
+
+    apiService.query = searchName;
+
+    apiService.resetPage();
 
     try {
-        fetchGallery(serchName).then(data => {
-            markupGallery(data)})
-    } catch (error) {
-          console.log(error);
-    }  
-}
+      const response = await apiService.fetchGallery();
+      const totalHits = response.data.totalHits;
+      const hits = response.data.hits;
+
+      if (hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        e.target.reset();
+        clearContainer();
+        return;
+      }
+      else {
+        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+      clearContainer();
+
+      galleryItems.innerHTML = markupGallery(hits);
+      simpleLightbox.refresh();
+      if (Math.ceil(totalHits / 40) === apiService.page) {
+        console.log(totalHits);
+        setTimeout(() => {
+          Notiflix.Notify.info(
+            "We're sorry, but you've reached the end of search results."
+          );
+        }, 2000);
+        return;
+      } else {
+        buttonShow();
+      }
+      }
+      } catch (error) {
+        console.error(error);
+      }
+    }
 
 async function onLoadMoreButton() {
-    try {
-        fetchGallery(serchName).then(data => {
-            markupGallery(data)})
-    } catch (error) {
-          console.log(error);
-    } 
+  apiService.page += 1;
+
+  try {
+    const response = await apiService.fetchGallery();
+    const hits = response.data.hits;
+    const totalHits = response.data.totalHits;
+
+    galleryItems.insertAdjacentHTML('beforeend', markupGallery(hits));
+    simpleLightbox.refresh();
+    slowScroll();
+    if (Math.ceil(totalHits / 40) === apiService.page) {
+      setTimeout(() => {
+        buttonHidden();
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }, 2000);
+    }
+    
+  } catch (error) {
+    console.error(error);
   }
-
-function markupGallery(data) {
-  const createMarkup = data.map(({webformatURL, tags, likes, views, comments, downloads}) => {
-    return `<div class="photo-card">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-    <div class="info">
-      <p class="info-item">
-        <b>${likes}</b>
-      </p>
-      <p class="info-item">
-        <b>${views}</b>
-      </p>
-      <p class="info-item">
-        <b>${comments}</b>
-      </p>
-      <p class="info-item">
-        <b>${downloads}</b>
-      </p>
-    </div>
-  </div>`  
-  }).join('');
-  galleryItems.insertAdjacentHTML('afterbegin', createMarkup);
-
 }
 
 
+function markupGallery(data) {
+  const createMarkup = data.map(
+    ({
+      largeImageURL,
+      webformatURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    }) => {
+      return `<div class="photo-card">
+              <a href="${largeImageURL}"><img class="photo" src="${webformatURL}" alt="${tags}" title="${tags}" loading="lazy"/></a>
+              <div class="info">
+                  <p class="info-item">
+                      <b>Likes</b> <span class="info-item-api"> ${likes} </span>
+                  </p>
+                  <p class="info-item">
+                      <b>Views</b> <span class="info-item-api">${views}</span>  
+                  </p>
+                  <p class="info-item">
+                      <b>Comments</b> <span class="info-item-api">${comments}</span>  
+                  </p>
+                  <p class="info-item">
+                      <b>Downloads</b> <span class="info-item-api">${downloads}</span> 
+                  </p>
+              </div>
+              </div>`;
+    }
+  )
+  .join('');
+return createMarkup;
+}
+
+function clearContainer() {
+  galleryItems.innerHTML = '';
+}
+
+function buttonHidden() {
+  loadMoreButton.classList.add('visually-hidden');
+}
+
+function buttonShow() {
+  setTimeout(() => {
+    loadMoreButton.classList.remove('visually-hidden');
+  }, 3000);
+}
+
+function slowScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.photo-card')
+    .firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
--- src/js/fetchGallery.js
+++ src/js/fetchGallery.js
@@ -1,24 +1,43 @@
-export default fetchGallery;
-import Notiflix from 'notiflix';
 import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/'
 const KEY_API = '34474183-c32a902a8a30cad0815e71cb5';
-let page = 1;
 
-async function fetchGallery(name) {
-try { 
-    const response = await axios.get(`${BASE_URL}?key=${KEY_API}&q=${name}`,  {
-    params: {
-      per_page: 4,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
+export default class SearchApiPixabay {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchGallery() {
+    try {
+      return await axios.get(BASE_URL, {
+        params: {
+          key: KEY_API,
+          q: this.searchQuery,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: 'true',
+          page: this.page,
+          per_page: 40,
+        },
+      });
+    } catch (error) {
+      console.error(error);
     }
-  })
-    return response.data.hits;
-} catch (error) {
-    console.log(error);
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
 
-}
+
